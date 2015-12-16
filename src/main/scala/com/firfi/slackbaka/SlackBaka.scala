77a0907cff4d910fb553d28d8c1d0c6b698ef2a0
Igor Loskutov <igor.loskutoff@gmail.com>
2015-12-16 03:59:24 +0300

    private responces for workers

--- src/main/scala/com/firfi/slackbaka/SlackBaka.scala
+++ src/main/scala/com/firfi/slackbaka/SlackBaka.scala
@@ -3,11 +3,10 @@ package com.firfi.slackbaka
 import akka.actor.{Actor, Props, ActorSystem}
 import com.firfi.slackbaka.listeners.WelcomeListener
 import com.firfi.slackbaka.workers.{HistoryLoader, PonyLoader, BakaDispatcher}
-import slack.models.SlackEvent
+import slack.api.SlackApiClient
 import slack.rtm.SlackRtmClient
+import scala.util.{Try, Success, Failure}
 
-// Async
-import scala.concurrent.ExecutionContext.Implicits.global
 
 
 object SlackBaka {
@@ -15,21 +14,33 @@ object SlackBaka {
   case class ChatMessage(message: String, channel: String, user: String, ts: String)
 
   case class BakaResponse(message: String, channel: String)
+  case class PrivateResponse(message: String, user: String)
 
   val workers = Set() ++ HistoryLoader.getWorkers ++ PonyLoader.getWorkers
 
-  class BakaResponder(slackRtmClient: SlackRtmClient) extends Actor {
+  class BakaResponder(slackRtmClient: SlackRtmClient, slackApiClient: SlackApiClient) extends Actor {
+    import scala.concurrent.ExecutionContext.Implicits.global
     override def receive: Receive = {
       case BakaResponse(message, channel) =>
         slackRtmClient.sendMessage(channel, message)
+      case PrivateResponse(message, user) =>
+        slackApiClient.openIm(user) andThen {
+          case Success(c: String) =>
+            slackRtmClient.sendMessage(c, message)
+            slackApiClient.closeIm(c)
+          case Failure(f) => println(f)
+        }
+
     }
   }
 
   def main(args: Array[String]) {
     implicit val system = ActorSystem("Baka")
-    val client = SlackRtmClient(System.getenv("SLACK_TOKEN"))
+    val API_TOKEN = System.getenv("SLACK_TOKEN")
+    val client = SlackRtmClient(API_TOKEN)
+    val apiClient = SlackApiClient(API_TOKEN)
 
-    val responder = system.actorOf(Props(new BakaResponder(client)))
+    val responder = system.actorOf(Props(new BakaResponder(client, apiClient)))
 
     val dispatcher = system.actorOf(Props(new BakaDispatcher(workers.map((worker) => {
       system.actorOf(Props(worker, responder))
@@ -40,7 +51,7 @@ object SlackBaka {
 
     val state = client.state
     val generalId = state.getChannelIdForName("general")
-    generalId.map{id => client.addEventListener(system.actorOf(Props(new WelcomeListener(responder, id))))} // TODO generalise
+    generalId.map{id => client.addEventListener(system.actorOf(Props(new WelcomeListener(responder))))} // TODO generalise
   }
 }
 
--- src/main/scala/com/firfi/slackbaka/listeners/WelcomeListener.scala
+++ src/main/scala/com/firfi/slackbaka/listeners/WelcomeListener.scala
@@ -1,13 +1,15 @@
 package com.firfi.slackbaka.listeners
 
 import akka.actor.{ActorRef, Actor}
-import com.firfi.slackbaka.SlackBaka.BakaResponse
-import slack.models.{User, TeamJoin}
+import com.firfi.slackbaka.SlackBaka.{PrivateResponse, BakaResponse}
+import slack.api.SlackApiClient
+import slack.models.{UserTyping, User, TeamJoin}
 
-class WelcomeListener(responder: ActorRef, generalId: String) extends Actor {
+class WelcomeListener(responder: ActorRef) extends Actor {
   override def receive: Receive = {
-    case TeamJoin(user: User) => responder ! BakaResponse(
-      s"Hey ${user.name} and welcome to our community!", generalId
+    case TeamJoin(user: User) => responder ! PrivateResponse(
+      s"Hey ${user.name} and welcome to our community!", user.id
     )
+    // case UserTyping(channel: String, user: String) => responder ! PrivateResponse("test", "USERIDHERE")
   }
 }
